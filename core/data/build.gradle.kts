864364873555ccf055f126282082f2f30082a110
Amirreza Gholami <jv233114@gmail.com>
2025-06-10 04:41:01 +0330

    fix: fix module dependencies

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,7 +2,7 @@ import ir.amirroid.mafiauto.buildSrc.ProjectPaths
 
 plugins {
     alias(libs.plugins.local.android.library)
-    alias(libs.plugins.local.kotlin)
+    alias(libs.plugins.local.compose.multiplatform)
 }
 
 kotlin {
