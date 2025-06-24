329e1042a6d3d6ab021408cfd0f3b11d411c43de
Amirreza Gholami <jv233114@gmail.com>
2025-06-24 12:43:50 +0330

    fix: fix platform dialog for android

--- core/design-system/src/androidMain/kotlin/ir/amirroid/mafiauto/design_system/components/popup/PlatformPopup.android.kt
+++ core/design-system/src/androidMain/kotlin/ir/amirroid/mafiauto/design_system/components/popup/PlatformPopup.android.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
 
 
 @Composable
@@ -17,18 +22,26 @@ internal actual fun PlatformPopup(
     dismissOnClickOutside: Boolean,
     content: @Composable () -> Unit
 ) {
-    Popup(
+    Dialog(
         onDismissRequest = onDismissRequest,
-        properties = PopupProperties(
+        properties = DialogProperties(
             usePlatformDefaultWidth = false,
             dismissOnBackPress = dismissOnBackPress,
-            dismissOnClickOutside = dismissOnClickOutside
+            dismissOnClickOutside = dismissOnClickOutside,
+            decorFitsSystemWindows = false
         )
     ) {
+        (LocalView.current.parent as? DialogWindowProvider)?.window?.apply {
+            SideEffect {
+                setDimAmount(0f)
+                navigationBarColor = Color.Transparent.toArgb()
+                statusBarColor = Color.Transparent.toArgb()
+            }
+        }
         Box(
             Modifier
-                .fillMaxSize()
-                .imePadding(), contentAlignment = Alignment.Center
+                .imePadding()
+                .fillMaxSize(), contentAlignment = Alignment.Center
         ) {
             content.invoke()
         }
