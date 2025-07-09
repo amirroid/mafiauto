96389eee3823bb8cd0f56dd6aec00dc9f9fdd182
Amirreza Gholami <jv233114@gmail.com>
2025-07-09 17:06:11 +0330

    replace: replace simple gradient with blur gradient

--- feature/intro/src/commonMain/kotlin/ir/amirroid/mafiauto/intro/screen/IntroScreen.kt
+++ feature/intro/src/commonMain/kotlin/ir/amirroid/mafiauto/intro/screen/IntroScreen.kt
@@ -1,6 +1,6 @@
 package ir.amirroid.mafiauto.intro.screen
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,8 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -22,74 +20,65 @@ import androidx.compose.ui.unit.dp
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Outline
 import compose.icons.evaicons.outline.Settings
+import dev.chrisbanes.haze.ExperimentalHazeApi
+import dev.chrisbanes.haze.HazeInputScale
+import dev.chrisbanes.haze.HazeTint
+import dev.chrisbanes.haze.hazeEffect
+import dev.chrisbanes.haze.hazeSource
+import dev.chrisbanes.haze.rememberHazeState
 import ir.amirroid.mafiauto.common.compose.components.ScreenContent
 import ir.amirroid.mafiauto.common.compose.modifiers.allPadding
-import ir.amirroid.mafiauto.design_system.components.text.MText
 import ir.amirroid.mafiauto.design_system.components.button.MButton
 import ir.amirroid.mafiauto.design_system.components.button.MIconButton
 import ir.amirroid.mafiauto.design_system.components.icon.MIcon
-import ir.amirroid.mafiauto.theme.core.AppTheme
+import ir.amirroid.mafiauto.design_system.components.text.MText
 import ir.amirroid.mafiauto.resources.Resources
+import ir.amirroid.mafiauto.theme.core.AppTheme
 import ir.amirroid.mafiauto.theme.locales.LocalAppTheme
 import ir.amirroid.mafiauto.theme.theme.AppThemeUiModel
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
+const val blackPartFraction = .7f
+
+@OptIn(ExperimentalHazeApi::class)
 @Composable
 fun IntroScreen(
     onStartGame: () -> Unit,
     onSettingsClick: () -> Unit
 ) {
-    val colorScheme = AppTheme.colorScheme
-    val blackPartFraction = .7f
-    val theme = LocalAppTheme.current
-    val backgroundImage = remember(theme) { getBackgroundImageForTheme(theme) }
-
     ScreenContent {
         Box(
             contentAlignment = Alignment.BottomCenter,
-            modifier = Modifier
-                .fillMaxSize()
-                .paint(
-                    painter = painterResource(backgroundImage),
-                    contentScale = ContentScale.Crop
-                )
-                .drawWithCache {
-                    onDrawWithContent {
-                        val brush = Brush.verticalGradient(
-                            0f to Color.Transparent,
-                            blackPartFraction to colorScheme.background
-                        )
-                        drawRect(brush)
-                        drawContent()
-                    }
-                },
+            modifier = Modifier.fillMaxSize()
         ) {
+            Background()
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(1f - blackPartFraction)
                     .padding(32.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp),
                 horizontalAlignment = Alignment.Start
             ) {
                 MText(
                     text = stringResource(Resources.strings.introTitle),
-                    style = AppTheme.typography.h1,
-                    color = colorScheme.onBackground
+                    color = AppTheme.colorScheme.onBackground,
+                    style = AppTheme.typography.h1
                 )
 
                 MText(
                     text = stringResource(Resources.strings.introDescription),
                     style = AppTheme.typography.body,
-                    color = colorScheme.onBackground.copy(alpha = 0.9f)
+                    color = AppTheme.colorScheme.onBackground.copy(alpha = 0.9f),
+                    modifier = Modifier.padding(top = 4.dp)
                 )
 
                 MButton(
-                    onClick = onStartGame,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(48.dp)
+                        .padding(top = 16.dp)
+                        .height(48.dp),
+                    onClick = onStartGame
                 ) {
                     MText(
                         text = stringResource(Resources.strings.startNewGame),
@@ -99,8 +88,7 @@ fun IntroScreen(
             }
             MIconButton(
                 onClick = onSettingsClick,
-                modifier = Modifier.align(Alignment.TopStart)
-                    .statusBarsPadding().allPadding()
+                modifier = Modifier.align(Alignment.TopStart).statusBarsPadding().allPadding()
             ) {
                 MIcon(
                     imageVector = EvaIcons.Outline.Settings,
@@ -115,4 +103,40 @@ private fun getBackgroundImageForTheme(theme: AppThemeUiModel?) = when (theme) {
     AppThemeUiModel.BLUE -> Resources.drawable.backgroundBlue
     AppThemeUiModel.GREEN -> Resources.drawable.backgroundGreen
     else -> Resources.drawable.backgroundRed
+}
+
+@OptIn(ExperimentalHazeApi::class)
+@Composable
+private fun Background(
+    modifier: Modifier = Modifier
+) {
+    val colorScheme = AppTheme.colorScheme
+    val theme = LocalAppTheme.current
+    val backgroundImage = remember(theme) { getBackgroundImageForTheme(theme) }
+    val hazeState = rememberHazeState()
+
+    Image(
+        painter = painterResource(backgroundImage),
+        contentDescription = null,
+        modifier = modifier
+            .fillMaxSize()
+            .hazeSource(hazeState),
+        contentScale = ContentScale.Crop
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .hazeEffect(hazeState) {
+                mask = Brush.verticalGradient(
+                    0.6f to Color.Transparent,
+                    blackPartFraction to Color.Black
+                )
+                inputScale = HazeInputScale.Auto
+                blurEnabled = true
+                tints = listOf(
+                    HazeTint(colorScheme.background.copy(alpha = 0.3f))
+                )
+            }
+    )
 }
